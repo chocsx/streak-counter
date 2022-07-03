ef453bddd9598006c3a17f4352c07294993e07d6
Mateus Sobrinho <mateus.webphp@gmail.com>
2022-07-03 11:49:03 -0300

    test(function): add function which decides if increments or not

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,22 @@ interface Streak {
 
 const KEY = 'streak';
 
+function shouldIncrementOrResetStreakCount(
+  currentDate: Date,
+  lastLoginDate: string
+): 'increment' | undefined {
+  const difference = currentDate.getDate() - parseInt(lastLoginDate.split('/')[1])
+  if (difference === 1) return 'increment';
+
+  return undefined;
+}
+
 export function streakCounter(storage: Storage, date: Date): Streak {
   const streakInLocalStorage = storage.getItem(KEY);
   if (streakInLocalStorage) {
     try {
       const streak = JSON.parse(streakInLocalStorage || "");
-      const state = "increment";
+      const state = shouldIncrementOrResetStreakCount(date, streak.lastLoginDate)
       const SHOULD_INCREMENT = state === "increment";
 
       if(SHOULD_INCREMENT) {
@@ -25,7 +35,7 @@ export function streakCounter(storage: Storage, date: Date): Streak {
 
         return updateStreak;
       }
-      
+
       return streak;
     } catch (error) {
       console.error("Failed to parse streak from localStorage")
